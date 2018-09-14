3de86465857031090c100da112219ab4b5781f32
Vladimir Putyanis <putyanis@yandex.ru>
2018-09-14 19:01:55 +0300

    process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MainPage } from '../pages/main/main';
+import { AuthPage } from '../pages/auth/auth';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    MainPage,
+    AuthPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    MainPage,
+    AuthPage
   ],
   providers: [
     StatusBar,
--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import axios from 'axios';
 
 /**
  * Generated class for the MainPage page.
@@ -19,7 +20,28 @@ export class MainPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MainPage');
+    
+  }
+
+  login() {
+    // const ayax = axios.create();
+
+    // axios.get('https://www.ayax.ru/api/v1/District/?filter=lt=44.950045;ln=38.924620', {
+    //   auth: {
+    //     username: 'restapiuser',
+    //     password: 'tHt@F(*YYsjR7bV5U80c~x}72k-sFw'
+    //   }
+    // }).then(function(response){
+    //   console.log(response);
+    // });
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.ayax.ru/api/v1/District/?filter=lt=44.950045;ln=38.924620');
+    xhr.setRequestHeader('Authorization', 'Basic cmVzdGFwaXVzZXI6dEh0QEYoKllZc2pSN2JWNVU4MGN+eH03Mmstc0Z3');
+    xhr.addEventListener('load', function(response){
+      console.log(response);
+    });
+    xhr.send();
   }
 
 }
